2bbd58010fb0aec947b5b60d077b3b4a0d4464cc
kolade Afeez <daisedalok@gmail.com>
2021-02-18 12:41:11 +0100

    initial and final

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const NotFound = () => {
+const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    },[])
+
+
+    return ( 
+          <div className="not-found">
+              <h1> OOOps ....</h1>
+              <h2> This Page Cannot Be Found </h2>
+              <p> Go Back To The <Link href="/"><a>HomePage</a></Link></p>
+          </div>
+    
+     );
+}
+ 
+export default NotFound;
